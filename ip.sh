ec075dc665a9c590c0bc34d6b46d2d3b8821dd45
lab6git <greblalopata9@gmail.com>
2026-01-07 17:28:07 +0200

    rezolvare cerinta 3 si 4

--- ip.sh
+++ ip.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
-
-while read -r line;
-do
-        [ -z "$line" ] && break
-        #ignora comentariile
-        case "$line" in 
-                \#*)
-                        continue  
-                 ;;
-        esac
-        dns=$(awk '/^nameserver: / {print $2}' /etc/resolv.conf)
-        ip=$(echo "$line" | awk '{print $1}')
-        name=$(echo "$line" | awk '{print $2}')
-        check=$(nslookup $name $dns | awk '/^Address: / {print $2}' | tail -n 1)
+check_ip(){
+        local name=$1
+        local ip=$2
+        local server=$3
+        check=$(nslookup $name $server | awk '/^Address: / {print $2}' | tail -n 1)
         if [ "$ip" = "$check" ]; then 
                 echo "IP Address matches" 
         else
                 echo "Bogus IP for {$name} in /etc/hosts!"
         fi
-done 
+}
+name=$1
+ip=$2
+server=$3
+check_ip "$name" "$ip" "$server"
 
 exit 0
 
